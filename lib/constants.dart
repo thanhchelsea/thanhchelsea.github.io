7989b7cb67b829c8632554f7d7b7321abe67508a
thanhchelsea <61274695+thanhchelsea@users.noreply.github.com>
2020-11-26 15:27:41 +0700

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,71 @@
-import 'dart:ui';
+Q: What is Earthquake Network?
+A: Earthquake Network is the first Android application to detect earthquakes through a global network of smartphone and pad.
 
-class Constants {
-  // locale
-  static const String VI = "vi";
-  static const String EN = "en";
-  static const SUPPORT_LOCALE = [Locale(VI), Locale(EN)];
-}
+Q: How does it work?
+A: When the device detects the waves of a quake, the event is notified to a server along with your geographic position. The server decides whether a quake is happening and in case of a quake all the users with the app are notified in real time.
 
-class SPrefCache {
-  static const String PREF_KEY_LANGUAGE = "pref_key_language"; // luu ngon ngu
-  static const String TYPE_MAP = "type_map";
-  static const String LIST_PHONE = "list_phone";
-  static const String MESSAGE_SOS = "message_sos";
-}
+Q: Why it is useful?
+A: If you live not too close to the epicenter of the quake, you can be notified by the app before the quake waves reach you.
+
+Q: Which quakes can the app detect?
+A: Usually strong quakes that can cause damages.
+
+Q: All quakes are detected?
+A: Quakes are detected only if the area of the quake is covered by a network of smartphones with the app installed.
+
+Q: What happens if the smartphones of Earthquake Network detect a quake?
+A: You will receive a real-time notification of the quake. The notification will have a white star as icon and you will be able to see on map the location of the quake.
+
+Q: How do I use the app?
+A: Start the application and use your device as always. The application works as a seismometer only when the device is charging and the screen is off.
+
+Q: Must the smartphone be charging in order to receive the real-time alert?
+A: No, charging smartphones are used for detection but the alert is received by all smartphones at any time if Internet is available.
+
+Q: The GPS receiver must be active?
+A: No, location must be enabled in “battery saving” mode. The GPS is not needed by the app.
+
+Q: Does the app drain my battery?
+A: No. The accelerometer only runs when the device is charging.
+
+Q: I know that the smartphone network has detected an earthquake but I have not received any alert, why?
+A: Check that you do not have battery saver apps installed on your device, or that you have not enabled settings on your device that prevent apps from receiving real-time notifications.
+
+Q: I have a smartphone Huawei, do I have to do something special?
+A: Yes, you need to add the app to the list of protected apps and you have to allow it to run in background. Search on Google for instructions on your smartphone model.
+
+Q: Can I manually report a quake?
+A: Yes from the page “Reports” and following instructions. Users will receive real time notifications of manually reported quakes.
+
+Q: Where can I see reports from the other users?
+A: Click on the map from the page “Reports”. Star icons on map show the reports of the last 24h. Green star: mild intensity; yellow star: strong intensity; red star: very strong intensity; black star: quake declared as false by the other users.
+
+Q: I just reported a quake but it is not on map, why?
+A: Reports are shown only if the quake is reported by different users to avoid false alarm.
+
+Q: What the page “Seismic Networks” is all about?
+A: The page shows the list of quakes detected by national and global official seismic networks.
+
+Q: What happens if the smartphones of Earthquake Network detect a quake?
+A: You will receive a real-time notification of the quake. The notification will have a star icon either white, light blue or blue, and you will be able to see on map the location of the quake.
+
+Q: What does it mean the colour of the star?
+A: The star is white if the smartphone network was not able to estimate the earthquake intensity. It is light blue if the quake is mild and blue if the quake is strong.
+
+Q: Why the magnitude is not provided?
+A: For the moment the network is not able to estimate the magnitude in real time. The distinction between mild and strong quakes is also an experimental feature.
+
+Q: How can I limit the number of notifications from global networks?
+A: From the options page. You can choose the minimum magnitude, the distance of the quake and the global networks you want to receive notifications from.
+
+Q: Does the app collect personal information?
+A: No. Only the geographic position is anonymously collected and it is deleted from the server after 10 minutes if no earthquakes are detected.
+
+Q: Why should I keep Earthquake Network on my device?
+A: Because the real-time detection of quakes is possible if the app is installed on a large number of devices. Thanks for your cooperation!
+
+Q: Why this app has advertising?
+A: The app is part of the Earthquake Network project which is not funded. Income from advertising is used to sustain the project.
+
+Q: What is section “Seismometer status” in the main page?
+A: It is the page where you can see if your device is suitable to be a seismometer. Click the green and red lights for more details.
